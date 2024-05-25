76e7ab953e896ab971c6f4867649a24966962bbb
Sellami Kais <sellami@insa-11274>
2024-05-25 16:20:41 +0200

    fichiers de tests mis à jour

--- be-graphes-gui/src/main/java/org/insa/graphs/gui/simple/Launch.java
+++ be-graphes-gui/src/main/java/org/insa/graphs/gui/simple/Launch.java
@@ -76,28 +76,37 @@ public class Launch {
         }
         */
 
-        /*Tester les durées exec Dij et A star */
-        String carteFrance="/mnt/commetud/3eme Annee MIC/Graphes-et-Algorithmes/Maps/france.mapgr";
-        TestDuree(carteFrance);
+        /*Tester les durées d'éxecution de Dij et A star */
+        
+        //String carteFrance="/mnt/commetud/3eme Annee MIC/Graphes-et-Algorithmes/Maps/france.mapgr";
+        //TestDuree(carteFrance);
+       // String carteCarreDense="/mnt/commetud/3eme Annee MIC/Graphes-et-Algorithmes/Maps/carre-dense.mapgr";
+       // TestDuree(carteCarreDense);
+       // String carteToulouse="/mnt/commetud/3eme Annee MIC/Graphes-et-Algorithmes/Maps/toulouse.mapgr";
+       // TestDuree(carteToulouse);
+       // String carteBordeaux="/mnt/commetud/3eme Annee MIC/Graphes-et-Algorithmes/Maps/bordeaux.mapgr";
+       // TestDuree(carteBordeaux);
 
-
+       String carteFrance="/mnt/commetud/3eme Annee MIC/Graphes-et-Algorithmes/Maps/france.mapgr";
+       cheminLong(carteFrance);
          /** Tests rapides sur petites cartes */
         /*
         final String folderMapsPath = "/mnt/commetud/3eme Annee MIC/Graphes-et-Algorithmes/Maps/";
-        String[] liste={"toulouse.mapgr","bordeaux.mapgr","paris.mapgr","haute-garonne.mapgr"};
+        String[] liste={"toulouse.mapgr","bordeaux.mapgr","paris.mapgr"};
         String map;
         for(int i=0;i<liste.length;i++){
             map=liste[i];
             System.out.println(BLUE+map+RESET);
             test(folderMapsPath+map);        
-        }
-        */
+        }*/
+        
         
     }
 
     /**Test pour les durées de Dij vs Astart */
 
     public static void TestDuree(String map) throws FileNotFoundException, IOException{
+        System.out.println("La carte: "+map);
         final GraphReader reader = new BinaryGraphReader(
             new DataInputStream(new BufferedInputStream(new FileInputStream(map))));
         final Graph graph = reader.read();
@@ -106,7 +115,7 @@ public class Launch {
         Node destination ;
         origin = getRandomNode(graph);
         destination = getRandomNode(graph);
-        ShortestPathData data = new  (graph, origin, destination, ArcInspectorFactory.getAllFilters().get(0));
+        ShortestPathData data = new ShortestPathData(graph, origin, destination, ArcInspectorFactory.getAllFilters().get(0));
         DijkstraAlgorithm djikstra = new DijkstraAlgorithm(data);
         AStarAlgorithm aStar = new AStarAlgorithm(data);
 
@@ -123,6 +132,118 @@ public class Launch {
         System.out.println("    A start: "+timeAStar/1000+"s, "+(int)(timeAStar%1000)+" ms"+RESET);
     }
 
+
+    public static void cheminLong(String map)throws FileNotFoundException, IOException{
+        final GraphReader reader = new BinaryGraphReader(
+            new DataInputStream(new BufferedInputStream(new FileInputStream(map))));
+        final Graph graph = reader.read();
+
+        Node origin ;
+        Node destination ;
+        origin = getRandomNode(graph);
+        destination = getRandomNode(graph);
+        System.out.println(GREEN+"origine: "+ origin.getId());
+        System.out.println("destination: "+ destination.getId()+RESET);
+        for (ArcInspector inspector : ArcInspectorFactory.getAllFilters()){
+        //    ArcInspector inspector = ArcInspectorFactory.getAllFilters().get(0);
+            System.out.println("inspector: "+inspector.toString());
+            ShortestPathData data = new ShortestPathData(graph, origin, destination, inspector);
+            DijkstraAlgorithm djikstra = new DijkstraAlgorithm(data);
+            AStarAlgorithm aStar = new AStarAlgorithm(data);
+            ShortestPathSolution pathDij = djikstra.run();
+            ShortestPathSolution pathAstar = aStar.run();
+            if ( pathDij.getPath()==null && pathAstar.getPath()==null){
+                System.out.println("pas de chemin pour les 2 algos");   
+            } else {
+                if(inspector.getMode()==Mode.LENGTH){
+                    double lengthPortionDij=0, lengthPortionAstar=0;
+                    Node debutPortion=origin;
+                    Node finPortion=origin;
+                    for(int i=0; i<pathAstar.getPath().size();i+=pathAstar.getPath().size()/10){
+                        debutPortion=finPortion;
+                        if((i+pathAstar.getPath().size()/10)>=(pathAstar.getPath().size()-1)){
+                            finPortion=destination;
+                        }else{
+                            i+=pathAstar.getPath().size()/10;
+                            finPortion=pathAstar.getPath().getArcs().get(i).getDestination();
+                        }
+                        ShortestPathData dataPotrtion = new ShortestPathData(graph, debutPortion, finPortion, inspector);
+                        ShortestPathSolution pathPortionAstar=new AStarAlgorithm(dataPotrtion).run();
+                        lengthPortionAstar+=pathPortionAstar.getPath().getLength();
+
+                    }
+                    System.out.println("La distance en:\n"+RED+"A star est:\t"+pathAstar.getPath().getLength());
+                    System.out.println("A star portion est:\t"+lengthPortionAstar+RESET);
+                    double diff=Math.abs(lengthPortionAstar-pathAstar.getPath().getLength());
+                    System.out.println("La différence est "+diff+" représente: "+diff*100/pathAstar.getPath().getLength()+"%");
+
+                    debutPortion=origin;
+                    finPortion=origin;
+                    for(int i=0; i<pathDij.getPath().size();i+=pathDij.getPath().size()/10){
+                        debutPortion=finPortion;
+                        if((i+pathDij.getPath().size()/10)>=(pathDij.getPath().size()-1)){
+                            finPortion=destination;
+                        }else{
+                            i+=pathDij.getPath().size()/10;
+                            finPortion=pathDij.getPath().getArcs().get(i).getDestination();
+                        }
+                        ShortestPathData dataPotrtion = new ShortestPathData(graph, debutPortion, finPortion, inspector);
+                        ShortestPathSolution pathPorionDij=new DijkstraAlgorithm(dataPotrtion).run();
+                        lengthPortionDij+=pathPorionDij.getPath().getLength();
+
+                    }
+                    System.out.println("La distance en:\n"+RED+"Dij est:\t"+pathDij.getPath().getLength());
+                    System.out.println("Dij portion est:\t"+lengthPortionDij+RESET);
+                    diff=Math.abs(lengthPortionDij-pathDij.getPath().getLength());
+                    System.out.println("La différence est "+diff+" représente: "+diff*100/pathDij.getPath().getLength()+"%");
+                }
+                if(inspector.getMode()==Mode.TIME){
+                    double timePortionDij=0, timePortionAstar=0;
+                    Node debutPortion=origin;
+                    Node finPortion=origin;
+                    for(int i=0; i<pathAstar.getPath().size();i+=pathAstar.getPath().size()/10){
+                        debutPortion=finPortion;
+                        if((i+pathAstar.getPath().size()/10)>=(pathAstar.getPath().size()-1)){
+                            finPortion=destination;
+                        }else{
+                            i+=pathAstar.getPath().size()/10;
+                            finPortion=pathAstar.getPath().getArcs().get(i).getDestination();
+                        }
+                        ShortestPathData dataPotrtion = new ShortestPathData(graph, debutPortion, finPortion, inspector);
+                        ShortestPathSolution pathPortionAstar=new AStarAlgorithm(dataPotrtion).run();
+                        timePortionAstar+=pathPortionAstar.getPath().getMinimumTravelTime();
+                    
+                    }
+                    System.out.println("La distance en:\n"+RED+"A star est\t:"+pathAstar.getPath().getMinimumTravelTime());
+                    System.out.println("A star portion est:\t"+timePortionAstar+RESET);
+                    double diff=Math.abs(timePortionAstar-pathAstar.getPath().getMinimumTravelTime());
+                    System.out.println("La différence est "+diff+" représente: "+diff*100/pathAstar.getPath().getMinimumTravelTime()+"%");
+                
+                    debutPortion=origin;
+                    finPortion=origin;
+                    for(int i=0; i<pathDij.getPath().size();i+=pathDij.getPath().size()/10){
+                        debutPortion=finPortion;
+                        if((i+pathDij.getPath().size()/10)>=(pathDij.getPath().size()-1)){
+                            finPortion=destination;
+                        }else{
+                            i+=pathDij.getPath().size()/10;
+                            finPortion=pathDij.getPath().getArcs().get(i).getDestination();
+                        }
+                        ShortestPathData dataPotrtion = new ShortestPathData(graph, debutPortion, finPortion, inspector);
+                        ShortestPathSolution pathPorionDij=new DijkstraAlgorithm(dataPotrtion).run();
+                        timePortionDij+=pathPorionDij.getPath().getMinimumTravelTime();
+                    
+                    }
+                    System.out.println("Le temps en :\n"+RED+"Dij est:\t"+pathDij.getPath().getMinimumTravelTime());
+                    System.out.println("Dij portion est:"+timePortionDij+RESET);
+                    diff=Math.abs(timePortionAstar-pathDij.getPath().getMinimumTravelTime());
+                    System.out.println("La différence est "+diff+" représente: "+diff*100/pathDij.getPath().getMinimumTravelTime()+"%");
+                }
+                
+            }
+        }
+        
+    }
 
     public static void test(String map) throws FileNotFoundException, IOException{
         final GraphReader reader = new BinaryGraphReader(
@@ -147,20 +268,10 @@ public class Launch {
                 BellmanFordAlgorithm bellman= new BellmanFordAlgorithm(data);
                 AStarAlgorithm aStar = new AStarAlgorithm(data);
 
-                long timeDij=new Date().getTime();
                 ShortestPathSolution pathDij = djikstra.run();
-                timeDij=new Date().getTime()-timeDij;
-                long timeBel=new Date().getTime();
                 ShortestPathSolution pathBel = bellman.run();
-                timeBel=new Date().getTime()-timeBel;
-                long timeAStar= new Date().getTime();
                 ShortestPathSolution pathAstar = aStar.run();
-                timeAStar = new Date().getTime()-timeAStar;
 
-                System.out.println(RED+"Les temps d'éxecution sont:");
-                System.out.println("Bellman: "+timeBel/1000+"s, "+(int) (timeBel%1000)+" ms");
-                System.out.println("    Dij: "+timeDij/1000+"s, "+(int)(timeDij%1000)+" ms");
-                System.out.println("    A start: "+timeAStar/1000+"s, "+(int)(timeAStar%1000)+" ms"+RESET);
 
                 
                 
